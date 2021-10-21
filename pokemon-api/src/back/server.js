b0852c4f936e44a42528774a67f52f531e3bbf87
ElayGelbart <elaygelbart@gmail.com>
2021-10-21 11:08:51 +0300

    Get is done
    we can now use get request

--- pokemon-api/src/back/server.js
+++ pokemon-api/src/back/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 const Pokedex = require('pokedex-promise-v2');
@@ -10,15 +11,22 @@ app.listen(port, function () {
   console.log('app started');
 });
 
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 // route our app
 app.get(`/pokemon/get/:id`, async function (req, res) {
   const pokeObj = await P.getPokemonByName(req.params.id);
   res.send(pokeObj);
 });
 
-app.put(`/pokemon/catch/:id`, function (req, res) {
+app.put(`/pokemon/catch/:id`, async function (req, res) {
+  const pokeObj = await P.getPokemonByName(req.params.id);
+  console.log(fs.accessSync(`${__dirname}/../../users/${req.body.username}/${pokeObj.id}.json`))
   res.send(req.params);
 });
+
 
 app.delete(`/pokemon/release/:id`, function (req, res) {
   res.send(req.params);
