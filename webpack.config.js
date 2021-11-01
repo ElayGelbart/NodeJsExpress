3f65fb20d8872cb25e6a0bc6f773edceb9771857
ElayGelbart <elaygelbart@gmail.com>
2021-11-01 14:36:12 +0200

    config again

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-<<<<<<< HEAD
 const serverConfig = {
   target: 'node',
   entry: "./pokemon-api/src/back/server.js",
@@ -10,18 +9,12 @@ const serverConfig = {
     filename: 'server.node.js',
   },
 };
-=======
->>>>>>> dae34621ec4fdffaccca56bbda9900aa5475ae69
 
 const clientConfig = {
   target: 'web',
   entry: "./pokemon-api/src/front/index.js",
   output: {
-<<<<<<< HEAD
     path: path.resolve(__dirname, "/pokemon-api/src/dist"),
-=======
-    path: path.resolve(__dirname, "./pokemon-api/src/dist"),
->>>>>>> dae34621ec4fdffaccca56bbda9900aa5475ae69
     filename: "app.js",
   },
   plugins: [
