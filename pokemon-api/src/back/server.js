2ccd11335d6fc4381b47c37d778f7a90028470fc
ElayGelbart <elaygelbart@gmail.com>
2021-11-01 15:50:59 +0200

    change dir to html landing

--- pokemon-api/src/back/server.js
+++ pokemon-api/src/back/server.js
@@ -19,8 +19,8 @@ app.listen(process.env.PORT || 3000,
 app.use(cors());
 app.use('/info', userRoute);
 app.use('/pokemon', pokemonRoute);
-app.use('/', express.static(path.resolve('../dist'))); // serve main path as static dir
+app.use('/', express.static(path.resolve('../'))); // serve main path as static dir
 app.get('/', function (req, res) { // serve main path as static file
-  res.sendFile(path.resolve('../dist/index.html'))
+  res.sendFile(path.resolve('../index.html'))
 });
 app.use(errorHandler);
