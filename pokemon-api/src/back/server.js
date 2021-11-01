aad8c696f1e5137267b1b2dfa0d263aaaaf0ce31
ElayGelbart <elaygelbart@gmail.com>
2021-11-01 16:19:58 +0200

    dirs

--- pokemon-api/src/back/server.js
+++ pokemon-api/src/back/server.js
@@ -18,8 +18,8 @@ app.listen(process.env.PORT || 3000,
 
 app.use(express.static(path.join(__dirname, '../dist')))
 app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
   res.send("hello")
-  res.sendFile(__dirname + "../dist/index.html");
 });
 app.use(cors());
 app.use('/info', userRoute);
