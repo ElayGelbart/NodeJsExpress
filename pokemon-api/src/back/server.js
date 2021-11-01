49ae8dfed51bce43c45494217860ea34738215a0
ElayGelbart <elaygelbart@gmail.com>
2021-11-01 12:17:47 +0200

    adjust server

--- pokemon-api/src/back/server.js
+++ pokemon-api/src/back/server.js
@@ -11,10 +11,17 @@ const app = express();
 const port = 8080;
 
 // start the server
-app.listen(port, function () {
-  console.log(`listing to port ${port}`);
-});
+// app.listen(port, function () {
+//   console.log(`listing to port ${port}`);
+// });
+app.listen(process.env.PORT || 3000,
+  () => console.log("Server is running..."));
+
 app.use(cors());
+app.use('/', express.static(path.resolve('./dist'))); // serve main path as static dir
+app.get('/', function (req, res) { // serve main path as static file
+  res.sendFile(path.resolve('../dist/index.html'))
+});
 app.use('/info', userRoute);
 app.use('/', pokemonRoute);
 app.use(errorHandler);
